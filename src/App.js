46e430d14d9f98826bff5ed6de89532c1abe2d26
Raj Tushal <tushalkhatri902@gmail.com>
2024-10-26 14:56:20 +0500

    Describe the changes you made

--- src/App.js
+++ src/App.js
@@ -1,60 +1,27 @@
 import './App.css';
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 import Card from './components/Card';
 import Categories from './components/Categories';
 import Jobs from './components/Jobs';
-import ButtonAppBar from './components/Navbar';
-
-
 import {
   BrowserRouter,
   Routes,
   Route,
-  Link,
 } from "react-router-dom";
-import { Category } from '@mui/icons-material';
 
 function App() {
-
   return (
-    <>
-      <Navbar/>
+    <BrowserRouter>
+      <Navbar />
       <div className='hirmain'>
-
-      <BrowserRouter>
-      <Routes>
-        <Route path="/category" element={<Categories />} />
-        {/* <Route path="/jobs" element={<ButtonAppBar><Jobs /></ButtonAppBar>} /> */}
-      </Routes>
-    </BrowserRouter>
-
-
-      <Card/>
+        <Routes>
+          <Route path="/" element={<Categories />} />
+          <Route path="/jobs" element={<Jobs />} />
+        </Routes>
+        <Card />
       </div>
-
-
-
-    </>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-// function App() {
-//   return (
-//  <div>
-//   <NavBar/>
-//   <JobDetail />
-
-//   {/* <div>
-//     useEffect(function(){
-//       console.log("useeffect wala")
-      
-//     },[])
-//     fetch("https://dummyjson.com/users")
-//     .
-//   </div> */}
-  
-//  </div>
-//   );
-// }
